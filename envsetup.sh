f99df55b0be808c33fc986eef8b5bf9235c40584
Andrew Dodd <atd7@cornell.edu>
2013-09-28 15:26:50 -0400

    envsetup: Set BLISS_BUILD environment variable
    This is used to gate a few features, including backuptool
    
    Change-Id: I02fee9e0765cca88c833bb0514762aa3b14642e3

--- envsetup.sh
+++ envsetup.sh
@@ -567,6 +567,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     BLISS_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
@@ -587,7 +588,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the bliss model name
-            lunch bliss_$target-userdebug
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch bliss_$target-$variant
         fi
     fi
     return $?
