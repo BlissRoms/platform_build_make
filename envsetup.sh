06221bc0370dcb621aa4d9df909f0c752e4b9986
Andrew Dodd <atd7@cornell.edu>
2013-09-28 15:26:50 -0400

    envsetup: Set BLISS_BUILD environment variable
    This is used to gate a few features, including backuptool
    
    Change-Id: I02fee9e0765cca88c833bb0514762aa3b14642e3

--- envsetup.sh
+++ envsetup.sh
@@ -130,6 +130,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^bliss_") ; then
+       BLISS_BUILD=$(echo -n $1 | sed -e 's/^bliss_//g')
+    else
+       BLISS_BUILD=
+    fi
+    export BLISS_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -633,6 +641,7 @@ function lunch()
     fi
 
     local product=$(echo -n $selection | sed -e "s/-.*$//")
+    check_product $product
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
     build_build_var_cache
