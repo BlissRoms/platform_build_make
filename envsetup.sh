8b22ad5c63979d7af495109b7fe82e1c95b12dc5
Andrew Dodd <atd7@cornell.edu>
2013-09-28 15:26:50 -0400

    Set BLISS_BUILD environment variable
    This is used to gate a few features, including backuptool
    
    Change-Id: Iae5f4dd97078076504ba31dccce2eb32c3452758
    Reviewed-on: http://review.blissroms.com/3967
    Reviewed-by: Patrick Graber
    Tested-by: Patrick Graber

--- envsetup.sh
+++ envsetup.sh
@@ -129,6 +129,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^bliss_") ; then
+       DU_BUILD=$(echo -n $1 | sed -e 's/^du_//g')
+    else
+       DU_BUILD=
+    fi
+    export DU_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
