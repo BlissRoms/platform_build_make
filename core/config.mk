1753a23136b92fe621c3db550ee1b156f927e217
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-05 22:17:59 +0000

    build: Fix global selinux policy inclusion
    We're adding CM-specific policies to an overlay instead of patching
    external/sepolicy, so we need to make sure it's included to prevent
    misbehaving systems
    
    Change-Id: I7989a1093f9abc8360d492e73a860eb4afad10ad

--- core/config.mk
+++ core/config.mk
@@ -867,4 +867,8 @@ endif
 RSCOMPAT_32BIT_ONLY_API_LEVELS := 8 9 10 11 12 13 14 15 16 17 18 19 20
 RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include vendor/aosp/sepolicy/sepolicy.mk)
+
 include $(BUILD_SYSTEM)/dumpvar.mk
