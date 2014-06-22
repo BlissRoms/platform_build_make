75afc6ba49451bce6116ad4fe054d97a8217a2e8
JustArchi <JustArchi@JustArchi.net>
2014-06-22 14:37:30 +0200

    Build: Add support for specifying build variant in brunch/breakfast
    This little modification allows specifying build variant in brunch/
    breakfast commands. For example we can use "brunch shamu user" to build
    user variant instead of default user.
    
    When no extra argument is given, userdebug is default.
    
    Change-Id: If4169cee368aadd52996c34846b62aeadc2b0ff3
    
    Conflicts:
    	envsetup.sh
    
    Change-Id: Ia0bdbe58167e505af16de2025c13b653a65df23f

--- envsetup.sh
+++ envsetup.sh
@@ -565,6 +565,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     BLISS_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
@@ -585,7 +586,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the bliss model name
-            lunch bliss_$target-userdebug
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch bliss_$target-$variant
         fi
     fi
     return $?
