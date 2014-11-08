b6133b7905000ef41ff720f04f92ddd2e27097b6
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-08 23:03:53 +0000

    Remove nulls from the edify generator.
    These would break the scripts. WTF are they doing here?
    
    Change-Id: I0c52335a27f7ae974279fde1c2461b2dba24d263
    Reviewed-on: http://review.blissroms.com/4002
    Reviewed-by: Patrick Graber
    Tested-by: Patrick Graber

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -133,7 +133,7 @@ class EdifyGenerator(object):
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
     cmd = ('assert(' +
-           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+           ' || '.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
                          % (i, i) for i in device.split(",")]) +
            ');')
     self.script.append(cmd)
@@ -141,7 +141,7 @@ class EdifyGenerator(object):
   def AssertSomeBootloader(self, *bootloaders):
     """Asert that the bootloader version is one of *bootloaders."""
     cmd = ("assert(" +
-           " ||\0".join(['getprop("ro.bootloader") == "%s"' % (b,)
+           " || ".join(['getprop("ro.bootloader") == "%s"' % (b,)
                          for b in bootloaders]) +
            ");")
     self.script.append(self.WordWrap(cmd))
@@ -149,7 +149,7 @@ class EdifyGenerator(object):
   def AssertSomeBaseband(self, *basebands):
     """Assert that the baseband version is one of *basebands."""
     cmd = ("assert(" +
-           " ||\0".join(['getprop("ro.baseband") == "%s"' % (b,)
+           " || ".join(['getprop("ro.baseband") == "%s"' % (b,)
                          for b in basebands]) +
            ");")
     self.script.append(self._WordWrap(cmd))
@@ -438,4 +438,5 @@ class EdifyGenerator(object):
     else:
       data = open(input_path, "rb").read()
     common.ZipWriteStr(output_zip, "META-INF/com/google/android/update-binary",
-                       data, perms=0o755)
+                       data, perms=0755)
+
