bb1a2399cf26615e40fc0762de23cc464e5cac83
tobitege <tobiasteschner@googlemail.com>
2015-03-30 18:53:00 -0400

    build: Call OTA Generation script [4/4]
    Makefile: call otaxml script to auto-generate xml for ota in out folder
    cleanbuild: also remove ota.xml in out folder for make dirty
    
    Change-Id: Iaefc9dac784aeb3f56f0377b800ea050c33bd6bf
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/cleanbuild.mk
+++ core/cleanbuild.mk
@@ -228,6 +228,7 @@ current_build_config :=
 installclean_files := \
 	$(HOST_OUT)/obj/NOTICE_FILES \
 	$(HOST_OUT)/sdk \
+	$(PRODUCT_OUT)/ota*.xml \
 	$(PRODUCT_OUT)/*.img \
 	$(PRODUCT_OUT)/*.ini \
 	$(PRODUCT_OUT)/*.txt \
