11dfd61ed012e48eed63adf2e2a19ac19c3047f0
moepda <moepda@gmail.com>
2015-10-06 02:29:54 +0000

    Remove unnecessary code from script
    Change-Id: I006daab1a6abd41bb5ce68bf8a643643001e081b

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -717,13 +717,10 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print("Flashing SuperSU...")
     common.ZipWriteStr(output_zip, "supersu/supersu.zip",
                    ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
-    script.Mount("/system")
     script.FlashSuperSU()
   if block_based:
-    script.Unmount("/system")
-
-  script.ShowProgress(0.05, 5)
-  script.WriteRawImage("/boot", "boot.img")
+     script.Print("Flashing Boot Image...")
+     script.WriteRawImage("/boot", "boot.img")
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
