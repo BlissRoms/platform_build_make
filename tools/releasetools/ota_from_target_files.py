41be63f19ef8cafcfa665b0b07bb00553defd6b9
Dark Eyes <dark_eyes_@hotmail.com>
2015-12-15 11:28:19 -0500

    Add SuperSU to Bliss
    Change-Id: I9e22c91ca71b354f5af954f13d9da36d64061219

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -720,6 +720,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     if block_based:
       script.Unmount("/system")
 
+  script.Print("{*} Flashing SuperSU Zip")
+  common.ZipWriteStr(output_zip, "supersu/supersu.zip",
+                 ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
+  script.FlashSuperSU()
+
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
