4a5a76dfd1fc5df80902b6c09515284bcf08510b
Nathan Prevette <nprevette420@gmail.com>
2016-05-26 15:27:46 -0700

    Add Bliss to ROM Installation
    You  will need to add "ro.product.device" if it's missing
    
    Example
    ro.build.device=shamu
    
    placed inside "system.prop"
    
    Change-Id: Ie25e318dde08ca623fe3e5775bda3085e538a8a2

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -657,6 +657,21 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.SetPermissionsRecursive("/tmp/install", 0, 0, 0o755, 0o644, None, None)
   script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0o755, 0o755, None, None)
 
+  script.Print("********************************");
+  script.Print("*  ____  _     ___ ____ ____   *");
+  script.Print("* | __ )| |   |_ _/ ___/ ___|  *");
+  script.Print("* |  _ \| |    | |\___ \___ \  *");
+  script.Print("* | |_) | |___ | | ___) |__) | *");
+  script.Print("* |____/|_____|___|____/____/  *");
+  script.Print("*                              *");
+  script.Print("********************************");                              
+  script.Print("                                    ");
+  builddate = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+  device = GetBuildProp("ro.product.device", OPTIONS.info_dict)
+  script.Print("    Build date: %s"%(builddate));
+  script.Print("    Device: %s "%(device)); 
+  script.Print("******************************************");
+
   if OPTIONS.backuptool:
     script.Mount("/system")
     script.RunBackup("backup")
