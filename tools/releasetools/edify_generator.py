c2e593f583640583399ceac758e6cb2babef072b
LorDClockaN <lordclockan@gmail.com>
2016-06-12 14:35:03 +0200

    Add hosts and custom files persistance on flash
    We had all the files needed for this from an era
    long time ago (AOKP) lol.
    So I just added the persist command to be launched
    when Backup command is launched
    
    Change-Id: I54352e9485931c4c79bf144f58e355838d74ce67

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -159,6 +159,8 @@ class EdifyGenerator(object):
     self.script.append('package_extract_dir("supersu", "/tmp/supersu");')
     self.script.append('run_program("/sbin/busybox", "unzip", "/tmp/supersu/supersu.zip", "META-INF/com/google/android/*", "-d", "/tmp/supersu");')
     self.script.append('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/supersu/supersu.zip");')
+  def RunPersist(self, command):
+    self.script.append(('run_program("/tmp/install/bin/persist.sh", "%s");' % command))
 
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
