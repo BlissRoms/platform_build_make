SlimRoms/android_build

Fixup zip naming and roomservice
Change-Id: Ibc77861c068edefc35d6925d21f320c187bacd0b

tools/roomservice.py
@@ -108,11 +108,47 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
-def get_default_revision():
-    m = ElementTree.parse(".repo/manifest.xml")
+
+def get_default(manifest=None):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
     d = m.findall('default')[0]
-    r = d.get('revision')
+    return d
+
+
+def get_default_revision(manifest=None):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    r = get_default(manifest=m).get('revision')
     return r.replace('refs/heads/', '').replace('refs/tags/', '')
+
+
+def get_remote(manifest=None, remote_name=None):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    if not remote_name:
+        remote_name = get_default(manifest=m).get('remote')
+    remotes = m.findall('remote')
+    for remote in remotes:
+        if remote_name == remote.get('name'):
+            return remote
+
+
+def get_revision(manifest=None, p="android_build"):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    project = None
+    for proj in m.findall("project"):
+        if re.search(r"%s$" % p, proj.get("name")):
+            project = proj
+            break
+    if project is not None:
+        return get_default_revision(manifest=m)
+    revision = project.get('revision')
+    if revision:
+        return revision.replace('refs/heads/', '').replace('refs/tags/', '')
+    remote = get_remote(manifest=m, remote_name=project.get('remote'))
+    revision = remote.get('revision')
+    if not revision:
+        return get_default_revision(manifest=None)
+    return revision.replace('refs/heads/', '').replace('refs/tags/', '')
+
 
 def get_from_manifest(devicename):
     try:
@@ -122,7 +158,7 @@ def get_from_manifest(devicename):
         lm = ElementTree.Element("manifest")
 
     for localpath in lm.findall("project"):
-        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+        if re.search("device_.*_%s$" % device, localpath.get("name")):
             return localpath.get("path")
 
     # Devices originally from AOSP are in the main manifest...
@@ -133,7 +169,7 @@ def get_from_manifest(devicename):
         mm = ElementTree.Element("manifest")
 
     for localpath in mm.findall("project"):
-        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+        if re.search("device_.*_%s$" % device, localpath.get("name")):
             return localpath.get("path")
 
     return None
@@ -173,21 +209,29 @@ def add_to_manifest(repositories, fallback_branch = None):
         repo_name = repository['repository']
         repo_target = repository['target_path']
         if exists_in_tree(lm, repo_name):
-            print('SlimRoms/%s already exists' % (repo_name))
+            print('%s already exists' % (repo_name))
             continue
 
-        print('Adding dependency: SlimRoms/%s -> %s' % (repo_name, repo_target))
-        project = ElementTree.Element("project", attrib = { "path": repo_target,
-            "remote": "github", "name": "SlimRoms/%s" % repo_name })
+        if "/" not in repo_name:
+            repo_name = "%s/%s" % ("SlimRoms", repo_name)
+
+        print('Adding dependency: %s -> %s' % (repo_name, repo_target))
+
+        project = ElementTree.Element(
+            "project",
+            attrib={"path": repo_target,
+                    "remote": "github",
+                    "name": "%s" % repo_name}
+        )
 
         if 'branch' in repository:
-            project.set('revision',repository['branch'])
+            project.set('revision', repository['branch'])
         elif fallback_branch:
-            print("Using fallback branch %s for %s" % (fallback_branch, repo_name))
+            print("Using fallback branch %s for %s" %
+                  (fallback_branch, repo_name))
             project.set('revision', fallback_branch)
         else:
             print("Using default branch for %s" % repo_name)
-
         lm.append(project)
 
     indent(lm, 0)
@@ -243,12 +287,12 @@ if depsonly:
 else:
     for repository in repositories:
         repo_name = repository['name']
-        if repo_name.startswith("android_device_") and repo_name.endswith("_" + device):
+        if repo_name.startswith("device_") and repo_name.endswith("_" + device):
             print("Found repository: %s" % repository['name'])
             
-            manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
+            manufacturer = repo_name.replace("device_", "").replace("_" + device, "")
             
-            default_revision = get_default_revision()
+            default_revision = get_revision()
             print("Default revision: %s" % default_revision)
             print("Checking branch info")
             githubreq = urllib.request.Request(repository['branches_url'].replace('{/branch}', ''))

BlissRoms/platform_build_make

tools/roomservice.py
@@ -109,11 +109,47 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
-def get_default_revision():
-    m = ElementTree.parse(".repo/manifest.xml")
+
+def get_default(manifest=None):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
     d = m.findall('default')[0]
-    r = d.get('revision')
+    return d
+
+
+def get_default_revision(manifest=None):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    r = get_default(manifest=m).get('revision')
     return r.replace('refs/heads/', '').replace('refs/tags/', '')
+
+
+def get_remote(manifest=None, remote_name=None):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    if not remote_name:
+        remote_name = get_default(manifest=m).get('remote')
+    remotes = m.findall('remote')
+    for remote in remotes:
+        if remote_name == remote.get('name'):
+            return remote
+
+
+def get_revision(manifest=None, p="android_build"):
+    m = manifest or ElementTree.parse(".repo/manifest.xml")
+    project = None
+    for proj in m.findall("project"):
+        if re.search(r"%s$" % p, proj.get("name")):
+            project = proj
+            break
+    if project is not None:
+        return get_default_revision(manifest=m)
+    revision = project.get('revision')
+    if revision:
+        return revision.replace('refs/heads/', '').replace('refs/tags/', '')
+    remote = get_remote(manifest=m, remote_name=project.get('remote'))
+    revision = remote.get('revision')
+    if not revision:
+        return get_default_revision(manifest=None)
+    return revision.replace('refs/heads/', '').replace('refs/tags/', '')
+
 
 def get_from_manifest(devicename):
     try:
@@ -123,7 +159,7 @@ def get_from_manifest(devicename):
         lm = ElementTree.Element("manifest")
 
     for localpath in lm.findall("project"):
-        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+        if re.search("device_.*_%s$" % device, localpath.get("name")):
             return localpath.get("path")
 
     # Devices originally from AOSP are in the main manifest...
@@ -134,7 +170,7 @@ def get_from_manifest(devicename):
         mm = ElementTree.Element("manifest")
 
     for localpath in mm.findall("project"):
-        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+        if re.search("device_.*_%s$" % device, localpath.get("name")):
             return localpath.get("path")
 
     return None
@@ -174,21 +210,29 @@ def add_to_manifest(repositories, fallback_branch = None):
         repo_name = repository['repository']
         repo_target = repository['target_path']
         if exists_in_tree(lm, repo_name):
-            print('BlissRoms/%s already exists' % (repo_name))
+            print('%s already exists' % (repo_name))
             continue
 
-        print('Adding dependency: BlissRoms/%s -> %s' % (repo_name, repo_target))
-        project = ElementTree.Element("project", attrib = { "path": repo_target,
-            "remote": "github", "name": "BlissRoms/%s" % repo_name })
+        if "/" not in repo_name:
+            repo_name = "%s/%s" % ("BlissRoms", repo_name)
+
+        print('Adding dependency: %s -> %s' % (repo_name, repo_target))
+
+        project = ElementTree.Element(
+            "project",
+            attrib={"path": repo_target,
+                    "remote": "github",
+                    "name": "%s" % repo_name}
+        )
 
         if 'branch' in repository:
-            project.set('revision',repository['branch'])
+            project.set('revision', repository['branch'])
         elif fallback_branch:
-            print("Using fallback branch %s for %s" % (fallback_branch, repo_name))
+            print("Using fallback branch %s for %s" %
+                  (fallback_branch, repo_name))
             project.set('revision', fallback_branch)
         else:
             print("Using default branch for %s" % repo_name)
-
         lm.append(project)
 
     indent(lm, 0)
@@ -199,7 +243,15 @@ def add_to_manifest(repositories, fallback_branch = None):
     f.write(raw_xml)
     f.close()
 
+_fetch_dep_cache = []
+
+
 def fetch_dependencies(repo_path, fallback_branch = None):
+    global _fetch_dep_cache
+    if repo_path in _fetch_dep_cache:
+        return
+    _fetch_dep_cache.append(repo_path)
+
     print('Looking for dependencies')
     dependencies_path = repo_path + '/bliss.dependencies'
     syncable_repos = []
@@ -210,7 +262,11 @@ def fetch_dependencies(repo_path, fallback_branch = None):
         fetch_list = []
 
         for dependency in dependencies:
-            if not is_in_manifest("BlissRoms/%s" % dependency['repository']):
+            repo_name = dependency['repository']
+            if "/" not in repo_name:
+                repo_name = "%s/%s" % ("BlissRoms", repo_name)
+
+            if not is_in_manifest(repo_name):
                 fetch_list.append(dependency)
                 syncable_repos.append(dependency['target_path'])
 
@@ -244,12 +300,12 @@ if depsonly:
 else:
     for repository in repositories:
         repo_name = repository['name']
-        if repo_name.startswith("android_device_") and repo_name.endswith("_" + device):
+        if repo_name.startswith("device_") and repo_name.endswith("_" + device):
             print("Found repository: %s" % repository['name'])
             
-            manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
+            manufacturer = repo_name.replace("device_", "").replace("_" + device, "")
             
-            default_revision = get_default_revision()
+            default_revision = get_revision()
             print("Default revision: %s" % default_revision)
             print("Checking branch info")
             githubreq = urllib.request.Request(repository['branches_url'].replace('{/branch}', ''))
@@ -294,3 +350,4 @@ else:
             sys.exit()
 
 print("Repository for %s not found in the BlissRoms Github repository list. If this is in error, you may need to manually add it to your local_manifests/bliss_manifest.xml." % device)
+