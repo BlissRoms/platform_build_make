Flinny/android_build

build: Add "installboot" command to install boot images
 * For devices that lack fastboot, this command will copy the boot image
   to a running device, write it out to the correct partition with dd,
   copy the modules and set the correct permissions.
 * Field surveys have discovered that a specific CM device maintainer is
   handling two devices, one with boot partition p7 and one at p8.
 * Extensive research has unveiled the fact that mixing up these
   partitions across these two specific devices will cause
   corruption of the TZ firmware, which cannot be restored and results
   in an unrecoverable brick.
 * Automate the process so this idiot (me) stops breaking shit.

Change-Id: I0dc5449daf128181e2e349ea26ad5741cc87bfe7

envsetup.sh
@@ -1656,6 +1656,45 @@ function godir () {
     \cd $T/$pathname
 }
 
+function installboot()
+{
+    if [ ! -e "$OUT/recovery/root/etc/recovery.fstab" ];
+    then
+        echo "No recovery.fstab found. Build recovery first."
+        return 1
+    fi
+    if [ ! -e "$OUT/boot.img" ];
+    then
+        echo "No boot.img found. Run make bootimage first."
+        return 1
+    fi
+    PARTITION=`grep "^\/boot" $OUT/recovery/root/etc/recovery.fstab | awk {'print $3'}`
+    if [ -z "$PARTITION" ];
+    then
+        echo "Unable to determine boot partition."
+        return 1
+    fi
+    adb start-server
+    adb root
+    sleep 1
+    adb wait-for-device
+    adb remount
+    adb wait-for-device
+    if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
+    then
+        adb push $OUT/boot.img /cache/
+        for i in $OUT/system/lib/modules/*;
+        do
+            adb push $i /system/lib/modules/
+        done
+        adb shell dd if=/cache/boot.img of=$PARTITION
+        adb shell chmod 644 /system/lib/modules/*
+        echo "Installation complete."
+    else
+        echo "The connected device does not appear to be $SLIM_BUILD, run away!"
+    fi
+}
+
 function mka() {
     case `uname -s` in
         Darwin)

BlissRoms/platform_build_make

envsetup.sh
@@ -1656,6 +1656,45 @@ function godir () {
     \cd $T/$pathname
 }
 
+function installboot()
+{
+    if [ ! -e "$OUT/recovery/root/etc/recovery.fstab" ];
+    then
+        echo "No recovery.fstab found. Build recovery first."
+        return 1
+    fi
+    if [ ! -e "$OUT/boot.img" ];
+    then
+        echo "No boot.img found. Run make bootimage first."
+        return 1
+    fi
+    PARTITION=`grep "^\/boot" $OUT/recovery/root/etc/recovery.fstab | awk {'print $3'}`
+    if [ -z "$PARTITION" ];
+    then
+        echo "Unable to determine boot partition."
+        return 1
+    fi
+    adb start-server
+    adb root
+    sleep 1
+    adb wait-for-device
+    adb remount
+    adb wait-for-device
+    if (adb shell cat /system/build.prop | grep -q "ro.bliss.device=$BLISS_BUILD");
+    then
+        adb push $OUT/boot.img /cache/
+        for i in $OUT/system/lib/modules/*;
+        do
+            adb push $i /system/lib/modules/
+        done
+        adb shell dd if=/cache/boot.img of=$PARTITION
+        adb shell chmod 644 /system/lib/modules/*
+        echo "Installation complete."
+    else
+        echo "The connected device does not appear to be $BLISS_BUILD, run away!"
+    fi
+}
+
 function mka() {
     case `uname -s` in
         Darwin)