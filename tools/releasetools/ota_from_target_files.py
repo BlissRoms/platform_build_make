5b0609a8f13f54328d6e58dec6a69d73c787f7d6
LorDClockaN <lordclockan@gmail.com>
2017-01-31 21:15:02 -0700

    Build: Switch to Magisk as the built in root [2/2]
    Pretty much identical to https://github.com/DirtyUnicorns/android_build/commit/2871a7b10b1ae6b6bdda049e33421ab37057f4d1
    
    Modified and updated for TWRP 3.0.3-x by @nathanchance
    
    Change-Id: Ic0cc0b6e96723a87298edb5cded9f6ef93c05b9d
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -802,6 +802,14 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
      script.Print("Flashing Boot Image...")
      script.WriteRawImage("/boot", "boot.img")
 
+  if block_based:
+    script.Print(" ")
+    script.Print("Flashing Magisk...")
+    script.Print(" ")
+    common.ZipWriteStr(output_zip, "magisk/magisk.zip",
+                   ""+input_zip.read("SYSTEM/addon.d/magisk.zip"))
+    script.FlashMagisk()
+    script.Print(" ")
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 
