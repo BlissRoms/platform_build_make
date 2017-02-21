ac3ee9bb4b60915287bac65b7be9dd0f80214507
Tom Powell <zifnab@zifnab06.net>
2017-02-20 17:41:45 -0800

    core: include getb64key.py in otatools
    Change-Id: I9c7a802a04110ee2bf7740454341003576129e26
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1073,7 +1073,7 @@ endif;
 
   common.ZipWriteStr(output_zip, "system/build.prop",
                      ""+input_zip.read("SYSTEM/build.prop"))
-  common.ZipWriteStr(output_zip, "META-INF/org/potatoos/releasekey",
+  common.ZipWriteStr(output_zip, "META-INF/org/cosmicos/releasekey",
                      ""+input_zip.read("META/releasekey.txt"))
 
   # We haven't written the metadata entry, which will be done in
