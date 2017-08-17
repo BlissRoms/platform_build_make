7f2a093b2e10e71b45558ef30590db3cbbb46d01
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:20:29 +0200

    core: Include QCOM/MTK targets
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7

--- core/config.mk
+++ core/config.mk
@@ -965,6 +965,12 @@ ifneq ($(BLISS_BUILD),)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/bliss/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/bliss/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
