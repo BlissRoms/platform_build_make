4360109cb001f846e73d6144a72a1c326ce37504
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-20 08:25:23 -0700

    build: Automatically replace old-style kernel header includes with new header lib
    Since we do this via soong and the result is a header library, just replace all
    legacy include paths that point to the old header location with calls to the new
    header library. Since we no longer have the legacy include, we can also remove
    the additional dependency.
    
    This reverts commit fa798218e5b7f96e12ab3acc9d47a3f26a140777.
    
    Change-Id: I716955534e50831e6568ca01e480aa8b90075d92

--- core/config.mk
+++ core/config.mk
@@ -1189,9 +1189,6 @@ ifneq ($(BLISS_BUILD),)
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
-# Rules for QCOM targets
--include $(TOPDIR)vendor/*/build/core/qcom_target.mk
-
 # Rules for MTK targets
 -include $(TOPDIR)vendor/*/build/core/mtk_target.mk
 endif
