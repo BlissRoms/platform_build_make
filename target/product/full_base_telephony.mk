a4f7379d444d6b6509c6b65b107352b344ae65b9
dianlujitao <dianlujitao@lineageos.org>
2019-09-05 21:56:36 +0800

    product: Exclude AOSP apn list on Bliss build
    Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -26,5 +26,10 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+ifeq ($(BLISS_BUILD),)
+PRODUCT_COPY_FILES += \
+    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+endif
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
