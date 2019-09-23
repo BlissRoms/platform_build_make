f16158527f669e0e14b9d1348158ab05bb0ae639
Colin Cross <ccross@android.com>
2019-09-23 12:52:32 -0700

    Gather lunch choices before printing prompt
    Get the lunch choices before starting to print the lunch menu prompt
    so that it doesn't sit at `Lunch menu... pick a combo:` with no
    choices for a few seconds.
    
    Fixes: 138719635
    Test: lunch
    Change-Id: I2bb373ec037bff0ab1d7e4f5ff8a8bd3b57f3263

--- envsetup.sh
+++ envsetup.sh
@@ -581,6 +581,7 @@ function add_lunch_combo()
 function print_lunch_menu()
 {
     local uname=$(uname)
+    local choices=$(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES)
     echo
     echo "You're building on" $uname
     echo
@@ -588,7 +589,7 @@ function print_lunch_menu()
 
     local i=1
     local choice
-    for choice in $(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES)
+    for choice in $choices
     do
         echo "     $i. $choice"
         i=$(($i+1))
