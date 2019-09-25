d0fb1951a78f54afe7b5280d70969bfabc63e61d
Rashed97 <rashed@linux.com>
2019-09-25 03:09:14 -0600

    core: Only include Lineage SE policy if repo exists
    Change-Id: I28312dbf8fdcd1920625bfe7a14d04dcde776dcd

--- core/config.mk
+++ core/config.mk
@@ -1177,9 +1177,11 @@ include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
 ifneq ($(BLISS_BUILD),)
+ifneq ($(wildcard device/bliss/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-#$(eval include device/bliss/sepolicy/common/sepolicy.mk)
+$(eval include device/bliss/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file
