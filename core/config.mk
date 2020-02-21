e3d26273429b2f4feadfb2a39125ad91e43edb66
SahilSonar <sss.sonar2003@gmail.com>
2020-02-21 04:49:23 +0000

    core: Include BoardConfigBliss
    Change-Id: I31946e6b142c5dbeea9c372ef4d874c8f1163f31

--- core/config.mk
+++ core/config.mk
@@ -320,6 +320,11 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 # be device and hardware independent.
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
+ifneq ($(BLISS_BUILD),)
+include vendor/vendor/config/BoardConfigBliss.mk
+endif
+
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
