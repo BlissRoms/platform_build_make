60501d9a3af71925cb1495163e66aaa4805762fb
Jackeagle <jackeagle102@gmail.com>
2020-10-04 01:50:22 -0400

    build: Move default AOSP Apps to vendor
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Icc772d0a8c46a745e0b9bd0b79b242bfd99e93b0

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -32,10 +32,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.com.android.dataroaming=true \
 
-PRODUCT_PACKAGES += \
-    PhotoTable \
-    WallpaperPicker \
-
 ifeq ($(BLISS_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif
