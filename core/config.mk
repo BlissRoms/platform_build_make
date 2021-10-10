5111ff5b637ae4f3890a6717683c17520a15baae
Jackeagle <jackeagle102@gmail.com>
2021-10-10 13:30:57 -0400

    fixup!: fix vendor/bliss path
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I48ec5208d53cd48e37c6ed25990f4a78598c498a

--- core/config.mk
+++ core/config.mk
@@ -321,9 +321,8 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
 ifneq ($(BLISS_BUILD),)
-include vendor/vendor/config/BoardConfigBliss.mk
+include vendor/bliss/config/BoardConfigBliss.mk
 endif
-
 
 # The build system exposes several variables for where to find the kernel
 # headers:
