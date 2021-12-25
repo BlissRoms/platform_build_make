fbb4bf81539d5230e272e5eebc7870d9c30a6362
Jackeagle <jackeagle102@gmail.com>
2021-12-25 13:21:35 -0500

    config: Enable bliss sepolicy
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ie9473fe66a3b6d86d951b35ae72e1ba67640c116

--- core/config.mk
+++ core/config.mk
@@ -1170,7 +1170,7 @@ endif
 ifneq ($(BLISS_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-# $(eval include device/bliss/sepolicy/common/sepolicy.mk)
+$(eval include device/bliss/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
