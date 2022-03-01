c7668cdba7d8dc61e5033cbf391062b5b0cc1fe0
Jackeagle <jackeagle102@gmail.com>
2022-03-01 11:12:59 +0100

    fixup!: Add Bliss Build Support
    Ref: https://github.com/BlissRoms/platform_build_make/commit/94c2fbeecb84cee05dd8d8a8083d0f081eceae08
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I2a6fcab12b70cfd008e2561e7c059c87c26e1515

--- core/config.mk
+++ core/config.mk
@@ -316,6 +316,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+-include vendor/extra/BoardConfigExtra.mk
 ifneq ($(BLISS_BUILD),)
 include vendor/bliss/config/BoardConfigBliss.mk
 endif
@@ -1162,27 +1163,26 @@ dont_bother_goals := out \
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
+ifneq ($(BLISS_BUILD),)
+ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+endif
+endif
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
-endif
-
-ifneq ($(BLISS_BUILD),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/bliss/sepolicy/common/sepolicy.mk)
-
-# Include any vendor specific config.mk file
--include $(TOPDIR)vendor/*/build/core/config.mk
-
-# Include any vendor specific apicheck.mk file
--include $(TOPDIR)vendor/*/build/core/apicheck.mk
 endif
 
 -include external/linux-kselftest/android/kselftest_test_list.mk
 -include external/ltp/android/ltp_package_list.mk
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
+
+# Include any vendor specific config.mk file
+-include vendor/*/build/core/config.mk
 
 # Make RECORD_ALL_DEPS readonly.
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
