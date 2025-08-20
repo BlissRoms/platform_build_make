d356f979809bc61ae7488b9362633a962eeec439
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-20 21:16:59 +0800

    envsetup: lunch: Generate build-manifest.xml after roomservice finishes
    Change-Id: I3717820be4f40e74c2361dffff3f4c0103c72d70
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -600,6 +600,9 @@ function lunch()
         cd - > /dev/null
     fi
 
+    # Generate build-manifest.xml after roomservice finishes
+    vendor/bliss/build/tools/gen_build-manifest.sh
+
     # Validate the selection and set all the environment stuff
     _lunch_meat $product $release $variant
 }
